e598de84b4a93e89c84b15d5142314dd08512753
Michael Naumov <mnaoumov@gmail.com>
2024-07-10 17:06:09 -0600

    Add conversion commands

---
+++ src/Async.ts
@@ -0,0 +1,34 @@
+const RETRY_DELAY_IN_MILLISECONDS = 100;
+const TIMEOUT_IN_MILLISECONDS = 5000;
+
+export async function retryWithTimeout(asyncFn: () => Promise<boolean>, options = {
+  timeoutInMilliseconds: TIMEOUT_IN_MILLISECONDS,
+  retryDelayInMilliseconds: RETRY_DELAY_IN_MILLISECONDS
+}): Promise<void> {
+  await runWithTimeout(options.timeoutInMilliseconds, async () => {
+    while (true) {
+      if (await asyncFn()) {
+        console.debug("Retry completed successfully");
+        return;
+      }
+
+      console.debug(`Retry completed unsuccessfully. Trying again in ${options.retryDelayInMilliseconds} milliseconds`);
+      await delay(options.retryDelayInMilliseconds);
+    }
+  });
+}
+
+async function delay(milliseconds: number): Promise<void> {
+  await new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+}
+
+async function runWithTimeout<R>(timeoutInMilliseconds: number, asyncFn: () => Promise<R>): Promise<R> {
+  return await Promise.race([asyncFn(), timeout()]);
+
+  async function timeout(): Promise<never> {
+    await delay(timeoutInMilliseconds);
+    throw new Error(`Timed out in ${timeoutInMilliseconds} milliseconds`);
+  }
+}
--- src/BetterMarkdownLinksPlugin.ts
+++ src/BetterMarkdownLinksPlugin.ts
@@ -1,10 +1,13 @@
 import {
+  Notice,
   Plugin,
+  type LinkCache,
   type TFile
 } from "obsidian";
 import BetterMarkdownLinksPluginSettings from "./BetterMarkdownLinksPluginSettings.ts";
 import BetterMarkdownLinksPluginSettingsTab from "./BetterMarkdownLinksPluginSettingsTab.ts";
 import { around } from "monkey-around";
+import { getCacheSafe } from "./MetadataCache.ts";
 
 type GenerateMarkdownLinkFn = (file: TFile, sourcePath: string, subpath?: string, alias?: string) => string;
 
@@ -28,6 +31,18 @@ export default class BetterMarkdownLinksPlugin extends Plugin {
     this.register(() => {
       uninstaller();
     });
+
+    this.addCommand({
+      id: "convert-links-in-current-file",
+      name: "Convert links in current file",
+      checkCallback: this.convertLinksInCurrentFile.bind(this)
+    });
+
+    this.addCommand({
+      id: "convert-links-in-entire-vault",
+      name: "Convert links in entire vault",
+      callback: this.convertLinksInEntireVault.bind(this)
+    });
   }
 
   public async saveSettings(newSettings: BetterMarkdownLinksPluginSettings): Promise<void> {
@@ -42,12 +57,26 @@ export default class BetterMarkdownLinksPlugin extends Plugin {
     this._settings = BetterMarkdownLinksPluginSettings.load(await this.loadData());
   }
 
-  private generateMarkdownLink(file: TFile, sourcePath: string, subpath: string | undefined, alias: string | undefined, originalFn: GenerateMarkdownLinkFn): string {
+  private checkObsidianSettingsCompatibility(): boolean {
+    if (this._settings.ignoreIncompatibleObsidianSettings) {
+      return true;
+    }
+
     const useWikilinks = !this.app.vault.getConfig("useMarkdownLinks");
     const newLinkFormat = this.app.vault.getConfig("newLinkFormat");
 
-    if (!this._settings.ignoreIncompatibleObsidianSettings && (useWikilinks || newLinkFormat !== "relative")) {
-      console.warn("Your Obsidian settings are incompatible with the current plugin. Please disable \"Use [[Wikilinks]]\" and set \"New link format\" to \"Relative path to file\" in Obsidian settings.");
+    if (!useWikilinks && newLinkFormat === "relative") {
+      return true;
+    }
+
+    const message = "Your Obsidian settings are incompatible with the \"Better Markdown Links\" plugin . Please disable \"Use [[Wikilinks]]\" and set \"New link format\" to \"Relative path to file\" in Obsidian settings.";
+    console.warn(message);
+    new Notice(message);
+    return false;
+  }
+
+  private generateMarkdownLink(file: TFile, sourcePath: string, subpath: string | undefined, alias: string | undefined, originalFn: GenerateMarkdownLinkFn): string {
+    if (!this.checkObsidianSettingsCompatibility()) {
       return originalFn(file, sourcePath, subpath, alias);
     }
 
@@ -67,5 +96,92 @@ export default class BetterMarkdownLinksPlugin extends Plugin {
     } else {
       return `![](${linkText})`;
     }
+  }
+
+  private convertLinksInCurrentFile(checking: boolean): boolean {
+    const activeFile = this.app.workspace.getActiveFile();
+    if (!activeFile || activeFile.extension.toLowerCase() !== "md") {
+      return false;
+    }
+
+    if (!checking) {
+      this.convertLinksInFile(activeFile).catch(console.error);
+    }
+
+    return true;
+  }
+
+  private async convertLinksInFile(file: TFile): Promise<void> {
+    if (!this.checkObsidianSettingsCompatibility()) {
+      return;
+    }
+
+    const cache = await getCacheSafe(this.app, file);
+    const content = await this.app.vault.read(file);
+    let newContent = "";
+    let lastIndex = 0;
+
+    const links = (cache.links ?? []).concat(cache.embeds ?? []).sort((a, b) => a.position.start.offset - b.position.start.offset);
+
+    for (const link of links) {
+      newContent += content.slice(lastIndex, link.position.start.offset);
+      newContent += this.convertLink(link, file);
+      lastIndex = link.position.end.offset;
+    }
+
+    newContent += content.slice(lastIndex);
+    await this.app.vault.modify(file, newContent);
+  }
+
+  private async convertLinksInEntireVault(): Promise<void> {
+    if (!this.checkObsidianSettingsCompatibility()) {
+      return;
+    }
+
+    const mdFiles = this.app.vault.getMarkdownFiles().sort((a, b) => a.path.localeCompare(b.path));
+
+    let index = 0;
+
+    const notice = new Notice("", 0);
+
+    for (const file of mdFiles) {
+      index++;
+      const message = `Converting links in note # ${index} / ${mdFiles.length}: ${file.path}`;
+      notice.setMessage(message);
+      console.log(message);
+      try {
+        await this.convertLinksInFile(file);
+      }
+      catch (e) {
+        console.error(e);
+      }
+    }
+
+    notice.hide();
+  }
+
+  private convertLink(link: LinkCache, source: TFile): string {
+    const [linkPath = "", originalSubpath] = link.link.split("#");
+
+    const linkFile = this.app.metadataCache.getFirstLinkpathDest(linkPath, source.path);
+    if (!linkFile) {
+      return link.original;
+    }
+
+    const subpath = originalSubpath ? "#" + originalSubpath : undefined;
+
+    const newLink = this.app.fileManager.generateMarkdownLink(linkFile, source.path, subpath, link.displayText);
+    const isLinkEmbed = link.original.startsWith("!");
+    const isNewLinkEmbed = newLink.startsWith("!");
+
+    if (isLinkEmbed === isNewLinkEmbed) {
+      return newLink;
+    }
+
+    if (isLinkEmbed) {
+      return "!" + newLink;
+    }
+
+    return newLink.replace("![]", `[${link.displayText}]`);
   }
 }
