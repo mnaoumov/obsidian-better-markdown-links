78becae0ecbfd91591b9da7330afde38990e43e4
Michael Naumov <mnaoumov@gmail.com>
2024-07-18 20:23:20 -0600

    Improve retry logging. Increase timeout to 30 sec

--- src/MetadataCache.ts
+++ src/MetadataCache.ts
@@ -31,7 +31,7 @@ export async function getCacheSafe(app: App, file: TFile): Promise<CachedMetadat
         return true;
       }
     }
-  });
+  }, { timeoutInMilliseconds: 30000 });
 
   return cache!;
 }
