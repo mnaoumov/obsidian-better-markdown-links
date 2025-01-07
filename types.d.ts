00ebc410ca9b32c6a3c1be9ebabaec3ea3eee815
Michael Naumov <mnaoumov@gmail.com>
2025-01-08 01:24:27 +0200

    Update options definition

--- types.d.ts
+++ types.d.ts
@@ -10,70 +10,70 @@ type GenerateMarkdownLinkFn = (options: GenerateMarkdownLinkOptions) => string
 /**
  * Options for generating a markdown link.
  */
-interface GenerateMarkdownLinkOptions {
-  /**
-   * The file to link to.
-   */
-  pathOrFile: string | TFile;
-
-  /**
-   * The source path of the link.
-   */
-  sourcePathOrFile: string | TFile;
-
-  /**
-   * The subpath of the link.
-   */
-  subpath?: string | undefined;
-
+export interface GenerateMarkdownLinkOptions {
   /**
    * The alias for the link.
    */
-  alias?: string | undefined;
+  alias?: string;
 
   /**
    * Indicates if the link should be embedded. If not provided, it will be inferred based on the file type.
    */
-  isEmbed?: boolean | undefined;
+  isEmbed?: boolean;
+
+  /**
+   * Whether to allow an empty alias for embeds. Defaults to `true`.
+   */
+  isEmptyEmbedAliasAllowed?: boolean;
+
+  /**
+   * Whether to allow non-existing files. If `false` and `pathOrFile` is a non-existing file, an error will be thrown. Defaults to `false`.
+   */
+  isNonExistingFileAllowed?: boolean;
 
   /**
    * Indicates if the link should be a wikilink. If not provided, it will be inferred based on the Obsidian settings.
    */
-  isWikilink?: boolean | undefined;
-
-  /**
-   * Indicates if the link should be relative. If not provided or `false`, it will be inferred based on the Obsidian settings.
-   */
-  forceRelativePath?: boolean | undefined;
-
-  /**
-   * Indicates if the link should use a leading dot. Defaults to `false`. Has no effect if `isWikilink` is `true` or `isRelative` is `false`.
-   */
-  useLeadingDot?: boolean | undefined;
-
-  /**
-   * Indicates if the link should use angle brackets. Defaults to `false`. Has no effect if `isWikilink` is `true`
-   */
-  useAngleBrackets?: boolean | undefined;
+  isWikilink?: boolean;
 
   /**
     * The original link text. If provided, it will be used to infer the values of `isEmbed`, `isWikilink`, `useLeadingDot`, and `useAngleBrackets`.
     * These inferred values will be overridden by corresponding settings if specified.
     */
-  originalLink?: string | undefined;
+  originalLink?: string;
 
   /**
-   * Whether to allow non-existing files. If `false` and `pathOrFile` is a non-existing file, an error will be thrown. Defaults to `false`.
+   * Indicates if the link should be relative. If not provided or `false`, it will be inferred based on the Obsidian settings.
    */
-  allowNonExistingFile?: boolean | undefined;
-
-  /**
-   * Whether to allow an empty alias for embeds. Defaults to `true`.
-   */
-  allowEmptyEmbedAlias?: boolean | undefined;
+  shouldForceRelativePath?: boolean;
 
   /**
    * Whether to include the attachment extension in the embed alias. Has no effect if `allowEmptyEmbedAlias` is `true`. Defaults to `false`.
    */
-  includeAttachmentExtensionToEmbedAlias?: boolean | undefined;
+  shouldIncludeAttachmentExtensionToEmbedAlias?: boolean;
+
+  /**
+   * Indicates if the link should use angle brackets. Defaults to `false`. Has no effect if `isWikilink` is `true`
+   */
+  shouldUseAngleBrackets?: boolean;
+
+  /**
+   * Indicates if the link should use a leading dot. Defaults to `false`. Has no effect if `isWikilink` is `true` or `isRelative` is `false`.
+   */
+  shouldUseLeadingDot?: boolean;
+
+  /**
+   * The source path of the link.
+   */
+  sourcePathOrFile: PathOrFile;
+
+  /**
+   * The subpath of the link.
+   */
+  subpath?: string;
+
+  /**
+   * The target path or file.
+   */
+  targetPathOrFile: PathOrFile;
 }
